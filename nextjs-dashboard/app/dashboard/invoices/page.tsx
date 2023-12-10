ed795b24f60d808fb009a0e2ecc173071163b3a6
RhettRoseman <rroseman@rcplanners.com>
2023-12-09 19:06:37 -0500

    next.js test

---
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Invoice(){
+    return( 
+        <html>
+        <p>Invoice Page</p>
+        </html>
+    )
+}
---
+++ nextjs-dashboard/app/ui/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+    <html><p>Dashboard Page</p>;</html> 
+    )
+  }
